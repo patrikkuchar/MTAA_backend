400e05ee470b012ae30804781fee1cc4e340e2b6
Patrik Kuchár <patrik.kuchar171@gmail.com>
2022-03-15 17:51:06 +0100

    first code commit

---
+++ app/views.py
@@ -0,0 +1,34 @@
+import psycopg2 as psycopg2
+import os
+from django.http import JsonResponse
+from django.shortcuts import render
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+conn = psycopg2.connect(database=os.getenv('DATABASE'), user=os.getenv('USER'),
+                        password=os.getenv('PASS'), host=os.getenv('HOST'),
+                        port=os.getenv('PORT'))
+cur = conn.cursor()
+
+# Create your views here.
+def homepage(request):
+    query = "SELECT * FROM users;"
+
+    cur.execute(query)
+
+    result = cur.fetchall()
+
+    object = {'object': []}
+
+    for row in result:
+        object["object"].append({
+            'id': row[0],
+            'name': row[1],
+            'surname': row[2],
+            'email': row[3],
+            'password': row[4]
+        })
+
+
+
+    return JsonResponse(object)
